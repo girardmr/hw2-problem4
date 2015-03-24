5d98893f80dc5b8cec67a6c512cda7c07161988a
girardmr <madeline.r.girard@vanderbilt.edu>
2015-03-23 19:21:26 -0500

    commit 3

--- hw2_problem4.m
+++ hw2_problem4.m
@@ -6,124 +6,128 @@ load('pointTargetData.mat');
 data = veraStrct.data;
 fs = 20e6;
 speed = 1540; %m/s in body
-pixel_size_through_depth = speed/fs; 
+pixel_size_through_depth = 0.5*(speed/fs); 
 
 for ii = 1:max(size(data))
     time_array_all(ii) = ii/fs;
 end
 
+for cc = 1:128
+for bb = 1:128
+    time_array(:,bb,cc) = time_array_all;
+end
+end
+
 channel = [[-63.5:1:63.5]];
 
+for beam = 1:128
+    
 for jj = 1:max(size(data)) %jj=row
     
 depth = jj*pixel_size_through_depth; %m
-if jj+150 <= max(size(data))
-    data_matrix = data([1:jj+150],:,:);
-elseif jj+150 > max(size(data))
-    data_matrix = data;
-end
+
+data_matrix = data;
 [rows_data_matrix col_data_matrix z_data_matrix] = size(data_matrix);
 
 for ii = 1:(length(channel))
     xe(ii) = 0.1953e-3*abs(channel(ii)); 
-    d(ii) = (xe(ii)^2+depth^2)^0.5;
+    d(ii) = (xe(ii)^2+depth^2)^0.5 + depth;
     time_to_point(ii) = d(ii)/speed;
 end
-time_from_zero = time_to_point(64);
-time_from_zero_v = ones(1,length(time_to_point))*time_from_zero;
-time_delay = time_to_point - time_from_zero_v;
 
-time_array = time_array_all(1:rows_data_matrix);
+delay_matrix(jj,:,beam) = time_to_point; %delays
+
+end
 
 for aa = 1:128
-    delay = time_delay(aa);
-    time_array_delayed = time_array+delay;
-    delayed_channel(1:rows_data_matrix,aa,:) = interp1(time_array,data_matrix(1:rows_data_matrix,aa,:),time_array_delayed,'linear');
-    delayed_channel_row(jj,aa,:) = delayed_channel(jj,aa,:);
+    delayed_channel(1:rows_data_matrix,aa,beam) = interp1(time_array(1:rows_data_matrix,aa,beam),data_matrix(1:rows_data_matrix,aa,beam),delay_matrix(1:rows_data_matrix,aa,beam),'linear');
 end
 
 
 end
 
-for ll = 1:numel(delayed_channel_row)
-    if isnan(delayed_channel_row(ll))==1
-        delayed_channel_row(ll) = 0;
+
+for ll = 1:numel(delayed_channel)
+    if isnan(delayed_channel(ll))==1
+        delayed_channel(ll) = 0;
     end
 end
 
 figure;
-min_data = min(min(min(delayed_channel_row)));
-max_data = max(max(max(delayed_channel_row)));
-imagesc(delayed_channel_row(:,:),[min_data, max_data])
+min_data = min(min(min(delayed_channel)));
+max_data = max(max(max(delayed_channel)));
+imagesc(delayed_channel(:,:),[min_data, max_data])
 colormap('gray');
-title('Channel data with delays (pointTargetData.mat)');
+title('Channel data with delays (pointTargetData.mat), Problem 4');
 
-summed_channels = sum(delayed_channel_row,2);
+summed_channels = sum(delayed_channel,2);
 figure;
 imagesc(20*log10(abs(hilbert(summed_channels(:,:)))));
 colormap('gray');
-title('Compressed B-mode image (pointTargetData.mat)');
+title('Compressed B-mode image (pointTargetData.mat), Problem 4');
 
 load('anecoicCystData.mat');
 
 data = veraStrct.data;
 fs = 20e6;
 speed = 1540; %m/s in body
-pixel_size_through_depth = speed/fs; 
+pixel_size_through_depth = 0.5*(speed/fs); 
 
 for ii = 1:max(size(data))
-    time_array(ii) = ii/fs;
+    time_array_all(ii) = ii/fs;
 end
 
-channel = [-63.5:1:63.5];
+for cc = 1:128
+for bb = 1:128
+    time_array(:,bb,cc) = time_array_all;
+end
+end
 
+channel = [[-63.5:1:63.5]];
+
+for beam = 1:128
+    
 for jj = 1:max(size(data)) %jj=row
     
 depth = jj*pixel_size_through_depth; %m
-if jj+150 <= max(size(data))
-    data_matrix = data([1:jj+150],:,:);
-elseif jj+150 > max(size(data))
-    data_matrix = data;
-end
+
+data_matrix = data;
 [rows_data_matrix col_data_matrix z_data_matrix] = size(data_matrix);
 
 for ii = 1:(length(channel))
     xe(ii) = 0.1953e-3*abs(channel(ii)); 
-    d(ii) = (xe(ii)^2+depth^2)^0.5;
+    d(ii) = (xe(ii)^2+depth^2)^0.5 + depth;
     time_to_point(ii) = d(ii)/speed;
 end
-time_from_zero = time_to_point(64);
-time_from_zero_v = ones(1,length(time_to_point))*time_from_zero;
-time_delay = time_to_point - time_from_zero_v;
 
-time_array = time_array_all(1:max(size(data_matrix)));
+delay_matrix(jj,:,beam) = time_to_point; %delays
+
+end
 
 for aa = 1:128
-    delay = time_delay(aa);
-    time_array_delayed = time_array+delay;
-    delayed_channel(1:rows_data_matrix,aa,:) = interp1(time_array,data_matrix(1:rows_data_matrix,aa,:),time_array_delayed,'linear');
-    delayed_channel_row(jj,aa,:) = delayed_channel(jj,aa,:);
+    delayed_channel(1:rows_data_matrix,aa,beam) = interp1(time_array(1:rows_data_matrix,aa,beam),data_matrix(1:rows_data_matrix,aa,beam),delay_matrix(1:rows_data_matrix,aa,beam),'linear');
 end
 
 
 end
 
-for ll = 1:numel(delayed_channel_row)
-    if isnan(delayed_channel_row(ll))==1
-        delayed_channel_row(ll) = 0;
+
+for ll = 1:numel(delayed_channel)
+    if isnan(delayed_channel(ll))==1
+        delayed_channel(ll) = 0;
     end
 end
 
 figure;
-min_data = min(min(min(delayed_channel_row)));
-max_data = max(max(max(delayed_channel_row)));
-imagesc(delayed_channel_row(:,:),[min_data, max_data])
+min_data = min(min(min(delayed_channel)));
+max_data = max(max(max(delayed_channel)));
+imagesc(delayed_channel(:,:),[min_data, max_data])
 colormap('gray');
-title('Channel data with delays (anecoicCystData.mat)');
+title('Channel data with delays (anecoicCystData.mat), Problem 4');
 
-summed_channels = sum(delayed_channel_row,2);
+summed_channels = sum(delayed_channel,2);
 figure;
 imagesc(20*log10(abs(hilbert(summed_channels(:,:)))));
 colormap('gray');
-title('Compressed B-mode image (anecoicCystData.mat)');
+title('Compressed B-mode image (anecoicCystData.mat), Problem 4');
 
